90b00ae73f8299f2b9a8d25f484eb2e848f6b6a3
MigRib16 <mf.ribeiro.0@gmail.com>
2022-02-05 19:07:49 +0000

    Teste

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
     MaterialApp(
     home : Scaffold(
       appBar: AppBar(
-        title: Text('Deticare'),
+        title: Text('Deticare App'),
         backgroundColor: Colors.blue,
       ),
     ),
