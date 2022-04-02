7c9bd93ba165bf09e2dc30834b7a6e67798f6570
MigRib16 <mf.ribeiro.0@gmail.com>
2022-04-02 01:00:14 +0100

    Upload de ficheiro

--- lib/Upload_Screen.dart
+++ lib/Upload_Screen.dart
@@ -1,8 +1,11 @@
+import 'dart:ui';
+
 import 'package:file_picker/file_picker.dart';
+import 'package:open_file/open_file.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
+//https://rrtutors.com/tutorials/Flutter-CSV-File-Import-and-Convert-CSV-File-to-List
 class UploadScreen extends StatelessWidget {
 const UploadScreen({Key? key}) : super(key: key);
 
@@ -15,10 +18,33 @@ const UploadScreen({Key? key}) : super(key: key);
           centerTitle: true,
           backgroundColor: Color.fromARGB(255, 31, 142, 245),
         ),
-        // Lets the user pick one file; files with any file extension can be selected
-FilePickerResult? result = await FilePicker.platform.pickFiles(
-  type: FileType.custom,
-  allowedExtensions: ['jpg', 'pdf', 'doc'],
-);
+        body: Container(
+          alignment: Alignment.center,
+          constraints: BoxConstraints(maxWidth: 1000),
+          padding: EdgeInsets.all(32),
+          width: 500.0,
+          height: 200.0,
+          child: ElevatedButton(
+            child: Text('Pick file'),
+             style: ElevatedButton.styleFrom(
+              primary: Color.fromARGB(255, 31, 142, 245),
+              fixedSize: const Size(240, 80),  //Tamanho do botão
+              shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50))),
+            onPressed: () async{
+              final result = await FilePicker.platform.pickFiles();
+              if(result==null) return;
+
+              //Open single file
+              final file=result.files.first;
+              openFile(file);
+            },
+          ),
+          ),
+
+       
       );
+void openFile(PlatformFile file){
+  OpenFile.open(file.path!);
+}
 }
